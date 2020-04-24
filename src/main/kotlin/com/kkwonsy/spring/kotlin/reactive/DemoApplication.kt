94306a159847398c7970e0d529cfc5c77f194752
sungyoungKwon85 <ssyang0@naver.com>
2020-04-24 13:59:44 +0900

    init project

---
+++ src/main/kotlin/com/kkwonsy/spring/kotlin/reactive/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.kkwonsy.spring.kotlin.reactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
